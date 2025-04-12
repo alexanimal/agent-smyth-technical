58c3fa2a400e40fd6e8917d3a5a50a439f8c848f
alexanimal <dcalex30@gmail.com>
2025-04-12 12:33:31 -0700

    Rename project from "ui" to "agent-smyth-technical" in package.json and update Vite configuration to dynamically set the base path based on the environment.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? `/${pkg.name}/` : '/',
   plugins: [
     react(),
     tsconfigPaths()
